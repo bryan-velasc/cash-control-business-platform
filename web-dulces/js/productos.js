023bc2155055904d38a8f334d3c84457b2f896a4
bryan-velasc <velascomicheell@gmail.com>
2026-06-15 20:48:40 -0600

    feat: agregar base temporal de productos

---
+++ web-dulces/js/productos.js
@@ -0,0 +1,182 @@
+const PRODUCTOS = [
+    {
+        id: 1,
+        nombre: "Cacahuate Japonés",
+        categoria: "botanas",
+        precio: 15,
+        imagen: "img/dulces/cacahuate-japones.jpg",
+        stock: 20,
+        activo: true
+    },
+    {
+        id: 2,
+        nombre: "Cacahuate Habanero",
+        categoria: "botanas",
+        precio: 15,
+        imagen: "img/dulces/cacahuate-habanero.jpg",
+        stock: 20,
+        activo: true
+    },
+    {
+        id: 3,
+        nombre: "Cacahuate Hotnug",
+        categoria: "botanas",
+        precio: 15,
+        imagen: "img/dulces/cacahuate-hotnug.jpg",
+        stock: 20,
+        activo: true
+    },
+    {
+        id: 4,
+        nombre: "Cacahuate Queso",
+        categoria: "botanas",
+        precio: 15,
+        imagen: "img/dulces/cacahuate-queso.jpg",
+        stock: 20,
+        activo: true
+    },
+    {
+        id: 5,
+        nombre: "Churritos",
+        categoria: "botanas",
+        precio: 12,
+        imagen: "img/dulces/churritos.jpg",
+        stock: 20,
+        activo: true
+    },
+    {
+        id: 6,
+        nombre: "Halls",
+        categoria: "dulces",
+        precio: 15,
+        imagen: "img/dulces/halls.jpg",
+        stock: 15,
+        activo: true
+    },
+    {
+        id: 7,
+        nombre: "Pelón Pelorico",
+        categoria: "dulces",
+        precio: 10,
+        imagen: "img/dulces/pelon-pelorico.jpg",
+        stock: 15,
+        activo: true
+    },
+    {
+        id: 8,
+        nombre: "Mazapán",
+        categoria: "dulces",
+        precio: 6,
+        imagen: "img/dulces/mazapan.jpg",
+        stock: 30,
+        activo: true
+    },
+    {
+        id: 9,
+        nombre: "Chicles",
+        categoria: "dulces",
+        precio: 2,
+        imagen: "img/dulces/chicles.jpg",
+        stock: 50,
+        activo: true
+    },
+    {
+        id: 10,
+        nombre: "Snickers",
+        categoria: "dulces",
+        precio: 25,
+        imagen: "img/dulces/snickers.jpg",
+        stock: 0,
+        activo: true
+    },
+    {
+        id: 11,
+        nombre: "Panditas Enchilados",
+        categoria: "dulces",
+        precio: 10,
+        imagen: "img/dulces/panditas-enchilados.jpg",
+        stock: 20,
+        activo: true
+    },
+    {
+        id: 12,
+        nombre: "Gusanitos",
+        categoria: "dulces",
+        precio: 10,
+        imagen: "img/dulces/gusanitos.jpg",
+        stock: 20,
+        activo: true
+    },
+    {
+        id: 13,
+        nombre: "Paleta Bombachile",
+        categoria: "dulces",
+        precio: 5,
+        imagen: "img/dulces/bombachile.jpg",
+        stock: 30,
+        activo: true
+    },
+    {
+        id: 14,
+        nombre: "Bombón de Chocolate",
+        categoria: "dulces",
+        precio: 15,
+        imagen: "img/dulces/bombon.jpg",
+        stock: 15,
+        activo: true
+    },
+    {
+        id: 15,
+        nombre: "Galletas Maravilla",
+        categoria: "galletas",
+        precio: 26,
+        imagen: "img/dulces/maravillas.jpg",
+        stock: 10,
+        activo: true
+    },
+    {
+        id: 16,
+        nombre: "Galletas Florentinas",
+        categoria: "galletas",
+        precio: 26,
+        imagen: "img/dulces/florentinas.jpg",
+        stock: 10,
+        activo: true
+    },
+    {
+        id: 17,
+        nombre: "Galletas Emperador",
+        categoria: "galletas",
+        precio: 26,
+        imagen: "img/dulces/emperador.jpg",
+        stock: 10,
+        activo: true
+    },
+    {
+        id: 18,
+        nombre: "Galletas Chokis",
+        categoria: "galletas",
+        precio: 26,
+        imagen: "img/dulces/chokis.jpg",
+        stock: 10,
+        activo: true
+    },
+    {
+        id: 19,
+        nombre: "Chocomilk",
+        categoria: "bebidas",
+        precio: 210,
+        imagen: "img/chocomiles/chocomilk.jpg",
+        stock: 5,
+        activo: true
+    },
+    {
+        id: 20,
+        nombre: "Calcetose",
+        categoria: "bebidas",
+        precio: 230,
+        imagen: "img/chocomiles/calcetose.jpg",
+        stock: 5,
+        activo: true
+    }
+];
