21827b87fb0f88b9fb4440674f5ca244e600151f
bryan-velasc <velascomicheell@gmail.com>
2026-06-21 18:36:47 -0600

    feat: conectar modulo Mi Negocio con productos desde API

--- cash-control-app/lib/modules/business/services/api_config.dart
+++ cash-control-app/lib/modules/business/services/api_config.dart
@@ -15,8 +15,7 @@ class ApiConfig {
     https://tu-backend-en-render.onrender.com
   */
 
-  static const String baseUrl = 'http://10.0.2.2:8000';
-
+  static const String baseUrl = 'http://127.0.0.1:8000';
   static const String adminToken = 'cama_75_pol_*';
 
   static Map<String, String> get adminHeaders {
